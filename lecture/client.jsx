4a43d0c87d7faa1411804cd0c14dee7de3a30461
HoyeongJeon <hoyeong0302@gmail.com>
2021-07-13 17:01:37 +0900

    가위바위보

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 // import { hot } from "react-hot-loader/root";
 
-import ResponseCheck from "./ResponseCheck";
+import RSP from "./RSP";
 
 // const Hot = hot(NumberBaseball);
 
-ReactDom.render(<ResponseCheck />, document.querySelector("#root"));
+ReactDom.render(<RSP />, document.querySelector("#root"));
