15f8d89245446822b42a06d4c4f68d3b45374a04
HoyeongJeon <hoyeong0302@gmail.com>
2021-09-07 16:14:11 +0900

    initialize

--- lecture/client.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-// import { hot } from "react-hot-loader/root";
-
-import RSP from "./RSP";
-
-// const Hot = hot(NumberBaseball);
-
-ReactDom.render(<RSP />, document.querySelector("#root"));
