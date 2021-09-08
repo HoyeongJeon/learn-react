9cb0b1c0c34aa3ace6470537df51aec374f45b62
HoyeongJeon <hoyeong0302@gmail.com>
2021-09-08 20:17:44 +0900

    로그인 onSubmit 추가

--- front/pages/Login/index.tsx
+++ front/pages/Login/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Header, Form, Label, Button, Input, LinkBox } from '@pages/Login/styles';
 import { Link } from 'react-router-dom';
 import useInput from '@hooks/useInput';
 
 const Login = () => {
-  const [email, onChangeEmail] = useInput('');
-  const [password, onChangePassword] = useInput('');
+  const [email, onChangeEmail, setEmail] = useInput('');
+  const [password, onChangePassword, setPassword] = useInput('');
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(email, password);
+      setEmail('');
+      setPassword('');
+    },
+    [email, password],
+  );
   return (
     <div id="container">
       <Header>DKUCIS</Header>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Label id="email-label">
           <span>이메일</span>
           <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail}></Input>
