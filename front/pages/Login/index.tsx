8ca88a8173deb352e521a17686447efa625ad846
HoyeongJeon <hoyeong0302@gmail.com>
2021-09-08 20:14:53 +0900

    로그인 페이지 기본적인 프론트

--- front/pages/Login/index.tsx
+++ front/pages/Login/index.tsx
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Header, Form, Label, Button, Input, LinkBox } from '@pages/Login/styles';
+import { Link } from 'react-router-dom';
+import useInput from '@hooks/useInput';
 
 const Login = () => {
-  return <h2>로그인</h2>;
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  return (
+    <div id="container">
+      <Header>DKUCIS</Header>
+      <Form>
+        <Label id="email-label">
+          <span>이메일</span>
+          <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail}></Input>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <Input type="password" id="password" name="password" value={password} onChange={onChangePassword}></Input>
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkBox>
+        회원이 아니신가요? &nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkBox>
+    </div>
+  );
 };
 
 export default Login;
